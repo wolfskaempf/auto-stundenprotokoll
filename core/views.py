c6c638277b96fd6c46b6b24fd7747ec9078f4326
wolfskaempf <tom@wolfskaempf.de>
2015-07-11 16:47:06 +0200

    added context to the home view

--- core/views.py
+++ core/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import Class, Student, Lesson
+
 def home(request):
+
+    classes = Class.objects.order_by(Class.name)
+
     template = "home.html"
-    context = {}
+    context = {"classes": classes}
     return render(request, template, context)
 
 def singleClass(request, pk):
