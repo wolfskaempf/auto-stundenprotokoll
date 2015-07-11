ee53e9708c76311ab81b79c245c23af8c766cd09
wolfskaempf <tom@wolfskaempf.de>
2015-07-11 13:23:25 +0200

    moved models for code consistency

--- core/models.py
+++ core/models.py
@@ -16,23 +16,25 @@ class Class(models.Model):
     class Meta:
         verbose_name_plural = "Classes"
 
+
+class Student(models.Model):
+    """ This model contains information about the student itself. """
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    inClass = models.ManyToManyField(Class) # This is the class the student is in. It's not possible to just use class as it conflicts with python.
+
+    def __unicode__(self):
+        # This function defines what the object will return when it's viewed as a whole.
+        return self.firstName + " " + self.lastName
+
 class LessonProtocol(models.Model):
     """ This model contains the protocol text of a given lesson in a class """
     inClass = models.ForeignKey(Class)
     protocolDate = models.DateTimeField(auto_now_add=True)
+    attendingStudents = models.ManyToManyField(Student)
     protocolText = models.TextField()
 
 
     def __unicode__(self):
         # This function defines what the object will return when it's viewed as a whole.
         return self.protocolText
-
-class Student(models.Model):
-    """ This model contains information about the student itself. """
-    firstName = models.CharField(max_length=100)
-    lastName = models.CharField(max_length=100)
-    inClass = models.ForeignKey(Class) # This is the class the student is in. It's not possible to just use class as it conflicts with python.
-
-    def __unicode__(self):
-        # This function defines what the object will return when it's viewed as a whole.
-        return self.firstName + " " + self.lastName
