ff378696adb380cbc7a74b088a334922e315231e
wolfskaempf <tom@wolfskaempf.de>
2015-07-11 13:11:44 +0200

    added new models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Class, LessonProtocol, Student
 # Register your models here.
+
+admin.site.register(Class)
+admin.site.register(LessonProtocol)
+admin.site.register(Student)
