a4b6fcf8c87e979951f1bf2d8bbf20a24cae6768
wolfskaempf <tom@wolfskaempf.de>
2015-07-11 18:41:49 +0200

    class and lesson data is now part of singleClass's context

--- core/views.py
+++ core/views.py
@@ -14,6 +14,10 @@ def home(request):
 
 def singleClass(request, pk):
     """ This view is supposed to be printed once a month for the archive. It should reset itself after every "lastPaid" of any given class """
+
+    activeClass = Class.objects.get(pk=pk)
+    lessons = Lesson.objects.filter(inClass=pk).order_by("date")
+
     template = "singleClass.html"
-    context = {}
+    context = {"class": activeClass, "lessons": lessons}
     return render(request, template, context)
