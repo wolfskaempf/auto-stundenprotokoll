8ac6f672fe6dba2280763a001031ebaad5adc71d
wolfskaempf <tom@wolfskaempf.de>
2015-07-11 16:11:37 +0200

    added home and singleClass views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    template = "home.html"
+    context = {}
+    return render(request, template, context)
+
+def singleClass(request, pk):
+    template = "singleClass.html"
+    context = {}
+    return render(request, template, context)
