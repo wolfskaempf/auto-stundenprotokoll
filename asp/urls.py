6ae2163a9ce4adfd050a1b86b005ff47beb685f3
wolfskaempf <tom@wolfskaempf.de>
2015-07-11 16:10:54 +0200

    added urls for home and singleClass

--- asp/urls.py
+++ asp/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from core import views
+
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^classes/(?P<pk>\d+)/$', views.singleClass, name='singleClass'),
     url(r'^admin/', include(admin.site.urls)),
 ]
