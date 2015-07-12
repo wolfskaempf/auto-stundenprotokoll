583000053284859818d04ee2386bb730c3928dce
wolfskaempf <tom@wolfskaempf.de>
2015-07-12 13:32:48 +0200

    added a LessonAdmin for proper admin display

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 from .models import Class, Lesson, Student
 # Register your models here.
 
+
+class LessonAdmin(admin.ModelAdmin):
+    # This defines what the Lesson Model should look like in the Django Admin
+    list_display = ["date", "inClass", "protocol",]
+    search_fields = ["date", "inClass", "protocol",]
+
+
+
+# The following lines are responsible for making the Model Data that's inside the database available in the Django admin
 admin.site.register(Class)
-admin.site.register(Lesson)
+admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Student)
