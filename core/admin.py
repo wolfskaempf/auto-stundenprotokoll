bbc7c48051dcf1be167a3bb019d0dfe008d58411
wolfskaempf <tom@wolfskaempf.de>
2015-07-12 14:19:54 +0200

    added ClassAdmin for proper display in the admin interface

--- core/admin.py
+++ core/admin.py
@@ -10,8 +10,14 @@ class LessonAdmin(admin.ModelAdmin):
     search_fields = ["date", "inClass", "protocol",]
 
 
+class ClassAdmin(admin.ModelAdmin):
+    # This defines what the Class Model should look like in the Django Admin
+    list_display = ["name", "subject", "identifier", "lastPaid", "teacher",]
+    search_fields = ["name", "subject", "identifier", "lastPaid", "teacher",]
+
+
 
 # The following lines are responsible for making the Model Data that's inside the database available in the Django admin
-admin.site.register(Class)
+admin.site.register(Class, ClassAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Student)
